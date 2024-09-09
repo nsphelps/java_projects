8b3a7251b57511c3fcde552b59d4651732f9ddcf
Nick Phelps <nicholas.phelps@proton.me>
2024-09-09 13:41:33 -0600

    Updating HelloWorld with print statement

--- HelloWorld/src/FirstClass.java
+++ HelloWorld/src/FirstClass.java
@@ -1,5 +1,6 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        System.out.print("Hello World!\n");
+        System.out.print("Its a nice day, isn't it?");
     }
 }
